16f29b32c7022813a5516db661fd23499b8fdde4
Andi Fahmi <andifahmi266@gmail.com>
2023-01-03 21:20:08 +0800

    Import Home To App.js

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import Home from './Src/Screens/Home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Welcome!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <Home />
+    </SafeAreaView>
   );
 }
 
